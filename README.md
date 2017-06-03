7dbe22ae41213145c7e86dff7294b07a2e1b1f29
Cass Fantastic <cass.fantastic@gmail.com>
2017-06-04 00:17:46 +1000

    Added time offset to get_n_frames, elaborated on readme

--- README.md
+++ README.md
@@ -8,22 +8,25 @@ Clone the repo, install ruby and ffmpeg, run `gem install bundler`, and then run
 
 ## Example
 
-Run `ruby extractframes.rb` to extract the first ten frames from the file `small.mp4` located in the `examples` folder. These will be created in the `examples` folder. This is not very interesting, as this example file only starts changing much after thirty frames or so. Oops.
+Run `ruby extractframes.rb` to extract the first ten frames from the file `small.mp4`, located in the `examples` folder, starting at 1 second into the video. These will be created in the `examples` folder as png files. 
 
 ## Usage
 
-Using `get_n_frames` will grab the first n frames in the specified video file, one at a time: 
+Using `get_n_frames` will grab the first `number_frames` frames in `video_filename`, one at a time, starting at `time_offset`, and save them in the given `format`. Provide the full filepath in `video_filename`. Returns the timestamp half-a-frame after the start of the frame immediately following the last frame saved. 
 ```ruby
 ExtractFrames.get_n_frames(
     video_filename: path_to_video,
-    n: number_of_frames
+    number_frames: number_of_frames,
+    time_offset: offset_in_seconds,
+    format: "png"
 )
 ```
-Using get_frame will grab the frame at the given timestamp, and save it in the given fileformat:
+
+Using get_frame will grab the frame in `video_filename` at the given `time_offset`, and save it in the given `format`. PNG or BMP is recommended here if you optionally want to use `number_frames` as an argument to get more than one frame from this point, as jpeg corruption got quite bad after a few frames.
 ```ruby
 ExtractFrames.get_frame(
     video_filename: path_to_video,
-    timestamp: starttime_in_seconds,
+    time_offset: offset_in_seconds,
     format: "png" 
 )
 ```
--- extractframes.rb
+++ extractframes.rb
@@ -3,38 +3,55 @@ require "open4"
 module ExtractFrames
     extend self
     
-    def get_n_frames(
-        video_filename: "#{__dir__}/examples/small.mp4",
-        n: 10
+    def get_fps(
+        video_filename,
     )
         fps = nil
-        #Run ffmpeg to get the fps of the video
         Open4::popen4 "ffmpeg -i #{video_filename}" do |pid, stdin, stdout, stderr| 
             output = stderr.read.strip.split(" ")
             fps = output[output.index("fps,")-1].to_f
         end
+        return fps
+    end
+
+    def get_n_frames(
+        video_filename: "#{__dir__}/examples/small.mp4",
+        number_frames: 10, 
+        time_offset: 1 ,
+        format: "png"
+    )
+        # number_frames: Number of frames to save (integer)
+        # time_offset: Time to start grabbing frames, in seconds (float) 
         
-        #Grab n frames, with timestamps in the middle of each frame period. 
-        for i in 0..(n-1)
-            start_time = (i+0.5)/fps
-            get_frame(video_filename: video_filename, timestamp: start_time)
-            puts "Saved frame #{i}!"
+        #Run ffmpeg to get the fps of the video
+        fps = get_fps(video_filename) 
+        
+        #Grab n frames, with timestamps in the middle of each frame period, starting at the frame beginning immediately prior to time_offset:
+        initial_frame = (initial_time*fps).floor
+        floored_time_offset = (initial_frame + 0.5)/fps
+        for i in 0..(number_frames-1)
+            start_time = floored_time_offset + i/fps
+            get_frame(video_filename: video_filename, timestamp: start_time, format: format)
+            puts "Saved frame #{i} from #{'%3f' % start_time.round(3)}s!"
         end
+        
+        # Returns the timestamp of the frame following the lasts frame grabbed
+        return (start_time+1/fps)
     end
 
     def get_frame(
         video_filename: "#{__dir__}/examples/small.mp4",
-        timestamp: 0,
+        time_offset: 0,
         number_frames: 1,
         format: "png"
     )
         # -i - input file
-        # -ss - seek to timestamp before starting recording
+        # -ss - seek to time_offset before starting recording
         # -frames - number of frames to record 
         # output filename formatting follows C printf, except integers only
         cmd = "ffmpeg "\
                 "-i #{video_filename} "\
-                "-ss #{timestamp} "\
+                "-ss #{time_offset} "\
                 "-frames #{number_frames} "\
                 "#{video_filename}-#{'%.3f' % timestamp.round(3)}-%03d.#{format}"
 
